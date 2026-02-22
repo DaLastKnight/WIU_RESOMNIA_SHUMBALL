0eed82132c7d0849a99d6313aa2716a8f5628170
LJH00G <lhj00g@gmail.com>
2026-02-22 17:19:12 +0800

    temp merge resolve
    require further resolving to update the scene to DemoScene's compatibility

--- DXGL-FRAMEWORK/Application/Source/Application.cpp
+++ DXGL-FRAMEWORK/Application/Source/Application.cpp
@@ -138,7 +138,7 @@ void Application::Run()
 {
 	srand(static_cast<unsigned int>(time(0)));
 	//Main Loop
-	Scene *scene = new SceneDemo();
+	Scene *scene = new SceneMedical();
 	scene->Init();
 
 	m_timer.startTimer();    // Start timer to calculate how long it takes to render this frame
